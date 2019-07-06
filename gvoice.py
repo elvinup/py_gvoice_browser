b0117de06321359b622f0caca6e8a42032f0bffc
Elvin Uthuppan <elvinutheman@gmail.com>
2019-07-05 22:34:02 -0400

    Made contact list click more accurate

--- gvoice.py
+++ gvoice.py
@@ -8,19 +8,19 @@ def login(driver, gmail, password):
     sleep(1)
     driver.find_element_by_xpath("//input[@type='email']").send_keys(gmail)
     driver.find_element_by_xpath("//span[contains(text(), 'Next')]").click()
-    sleep(1)
+    sleep(5)
     driver.find_element_by_xpath("//input[@type='password']").send_keys(password)
     driver.find_element_by_xpath("//span[contains(text(), 'Next')]").click()
 
 def send_text(driver, number, msg):
     driver.get("https://voice.google.com/u/0/messages")
-    sleep(1)
+    sleep(3)
     driver.find_element_by_xpath("//div[@ng-if='ctrl.showThreadListHeader()']").click()
     driver.find_element_by_xpath("//input[@type='search']").send_keys(number)
 
     #Select number
-    driver.find_element_by_xpath("//*[contains(text(), 'Send to')]").click()
-    sleep(1)
+    driver.find_element_by_xpath("//gv-contact-list").click()
+    sleep(3)
 
     #Write text
     driver.find_element_by_xpath("//textarea").send_keys(msg)
